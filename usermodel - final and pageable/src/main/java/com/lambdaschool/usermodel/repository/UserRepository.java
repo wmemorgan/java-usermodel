dd523099d65faa36f185a14f83ea349d35a83487
jrmmba8314 <jrmmba@outlook.com>
2019-11-11 08:21:25 -0800

    Adding Paging and Application Properties

---
+++ usermodel - final and pageable/src/main/java/com/lambdaschool/usermodel/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.usermodel.repository;
+
+import com.lambdaschool.usermodel.models.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+public interface UserRepository extends PagingAndSortingRepository<User, Long>
+{
+    User findByUsername(String username);
+
+    List<User> findByUsernameContainingIgnoreCase(String name,
+                                                  Pageable pageable);
+}

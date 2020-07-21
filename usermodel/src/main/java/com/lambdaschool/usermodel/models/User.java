84a41ec89e69f14be94e25bba53b57ff2d3614de
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-22 02:09:34 +0800

    Code along with User Auth guided project (Web31)

--- usermodel/src/main/java/com/lambdaschool/usermodel/models/User.java
+++ usermodel/src/main/java/com/lambdaschool/usermodel/models/User.java
@@ -1,7 +1,10 @@
 package com.lambdaschool.usermodel.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -172,8 +175,12 @@ public class User extends Auditable
      *
      * @param password the new password (String) for the user
      */
-    public void setPassword(String password)
-    {
+    public void setPassword(String password) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        this.password = passwordEncoder.encode(password);
+    }
+
+    public void setPasswordNoEncrypt(String password) {
         this.password = password;
     }
 
@@ -215,5 +222,17 @@ public class User extends Auditable
     public void setRoles(Set<UserRoles> roles)
     {
         this.roles = roles;
+    }
+
+    @JsonIgnore
+    public List<SimpleGrantedAuthority> getAuthority() {
+
+        List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
+        for (UserRoles r: this.roles) {
+            String myString = "ROLE_" + r.getRole().getName().toUpperCase();
+            rtnList.add(new SimpleGrantedAuthority(myString));
+        }
+
+        return rtnList;
     }
 }
